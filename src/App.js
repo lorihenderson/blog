91ec1a5b4f6a9e241685d9a00c004d908fbfdd9f
Lori Henderson <lorim95@gmail.com>
2023-07-24 18:13:56 -0400

    fixes emmet issue

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     hello world
-    </div>
+    <main>
+      <header>
+        not working,
+        tags won't show up
+        
+      </header>
+    </main>
   );
 }
 
